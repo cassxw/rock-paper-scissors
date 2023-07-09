a468a2f3b9509f7e1cb76075a02ec0633bb3cdb1
Cassandra Wallace <cassandra.s.wallace@gmail.com>
2023-07-09 18:38:57 +0200

    Finished attaching logic to each playerSelectionButton.

--- script.js
+++ script.js
@@ -5,13 +5,10 @@ function getComputerChoice() {
     {
         case 0:
             return "rock";
-            break;
         case 1:
             return "paper";
-            break;
         case 2:
             return "scissors";
-            break;
     }
 }
 
@@ -29,27 +26,33 @@ function playRound(playerSelection, computerSelection) {
         switch (playerSelection) {
             case "rock":
                 if (computerSelection == "paper") {
+                    computerScore++;
                     return "You lose! Paper beats Rock!";
                 }
                 else if (computerSelection == "scissors") {
+                    playerScore++;
                     return "You win! Rock beats Scissors!";
                 }
                 break;
 
             case "paper":
                 if (computerSelection == "rock") {
+                    playerScore++;
                     return "You win! Paper beats Rock!";
                 }
                 else if (computerSelection == "scissors") {
+                    computerScore++;
                     return "You lose! Scissors beats Paper!";
                 }
                 break;
 
             case "scissors":
                 if (computerSelection == "rock") {
+                    computerScore++;
                     return "You lose! Rock beats Scissors!";
                 }
                 else if (computerSelection == "paper") {
+                    playerScore++;
                     return "You win! Scissors beats Paper!";
                 }
                 break;
@@ -57,44 +60,18 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-// // Function that plays a 5 round game, keeping score and reports a winner or loser at the end.
-// function game()
-// {
-//     let playerWins = 0;
-//     let computerWins = 0;
+// Function that will update the results displayed on the page.
+function updateScores(result)
+{
+    document.getElementById("outcome").innerHTML = result;
+    document.getElementById("computerScore").innerHTML = "Computer Score: " + computerScore;
+    document.getElementById("playerScore").innerHTML = "Player Score: " + playerScore;
+}
 
-//     for (let i = 0; i < 5; i++)
-//     {
-//         let playerSelection = prompt("Enter your choice between either 'Rock', 'Paper' or 'Scissors': ");
-//         const computerSelection = getComputerChoice();
-//         console.log("The computer chose..." + computerSelection + "!");
-//         let result = playRound(playerSelection, computerSelection);
+let computerScore = 0;
+let playerScore = 0;
 
-//         if (result.includes("win"))
-//         {
-//             playerWins++;
-//         }
-//         else if (result.includes("lose"))
-//         {
-//             computerWins++;
-//         }
-
-//         console.log("Round " + (i+1) + ": " + result + "\n~ You: " + playerWins + " | Computer: " + computerWins);
-//     }
-
-//     if (playerWins == computerWins)
-//     {
-//         console.log("Draw! Wow...you are as good as a computer - Try again! :P")
-//     }
-//     else if (playerWins > computerWins)
-//     {
-//         console.log("Congratulations! You beat the computer! :D");
-//     }
-//     else
-//     {
-//         console.log("Uh oh...Sorry! The computer beat you - Try again! :(");
-//     }
-// }
-
-const buttons = document.querySelectorAll('.playerSelection');
-buttons.forEach(button => button.addEventListener('click', playRound));
+const buttons = document.querySelectorAll('.playerSelectionButton');
+buttons.forEach(button => button.addEventListener('click', () => {
+    updateScores(playRound(button.id, getComputerChoice()));
+}));
